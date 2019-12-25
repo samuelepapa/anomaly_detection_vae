Added runs and fixed KL through time.

ts_VI_LSTM.py
@@ -21,21 +21,29 @@ class Variational_LSTM(nn.Module):
         # encoder net, recognition model q(z_t+1|x_1:t)
         self.encoder_LSTM = nn.LSTM(input_size=input_dim,
                                     hidden_size=hidden_dim_rec,  # 2 because we want mu and sigma
-                                    num_layers=2
+                                    num_layers=1
                                     )
         self.encoder_hidden2hidden = nn.Linear(hidden_dim_rec, hidden_dim_rec)
 
         self.enc2latent = nn.Linear(hidden_dim_rec, 2 * latent_dim)
 
         # decoder net p(x_t+1|x_1:t,z_1:t)
+
         self.decoder_LSTM = nn.LSTM(input_size=input_dim + latent_dim,
                                     hidden_size=hidden_dim_gen,
-                                    num_layers=2
+                                    num_layers=1
                                     )
 
         self.decoder_hidden2hidden = nn.Linear(hidden_dim_gen, hidden_dim_gen)
 
         self.dec2features = nn.Linear(hidden_dim_gen, param_dist * input_dim)
+
+        self.decoder = nn.Sequential(
+            self.decoder_LSTM,
+            self.decoder_hidden2hidden,
+            nn.ReLU(),
+            self.dec2features
+        )
 
     def forward(self, x, device):
         outputs = {}
@@ -48,7 +56,8 @@ class Variational_LSTM(nn.Module):
         x_encoder = x.permute(1, 0, 2)
         encoder_out, (h, c) = self.encoder_LSTM(x_encoder)
         # add non-linearities and a level of abstraction
-        encoder_out = self.relu(self.encoder_hidden2hidden(encoder_out))
+        # encoder_out = self.relu(self.encoder_hidden2hidden(encoder_out))
+        # encoder_out = self.relu(encoder_out)
         # [seq_len, batch_dim, features]
         x_latent = self.enc2latent(encoder_out)  # outputs the latent variable parameters mu and sigma
 
@@ -61,8 +70,7 @@ class Variational_LSTM(nn.Module):
             epsilon = torch.randn(seq_length,
                                   batch_size,
                                   self.latent_dim,
-                                  # latent_samples here, but should only be one as we concatenate it to the oringinal x
-                                  ).to(device)  # 10 is the num of latent samples
+                                  ).to(device)
         # create the random latent variable, reparametrization trick
         z = mu + sigma * epsilon
 
@@ -81,7 +89,9 @@ class Variational_LSTM(nn.Module):
         # run it through the ordinary LSTM
         decoder_out, (h, c) = self.decoder_LSTM(x_aug)
         # symmetry with encoder
-        decoder_out = self.relu(self.decoder_hidden2hidden(decoder_out))
+        # decoder_out = self.relu(self.decoder_hidden2hidden(decoder_out))
+        # decoder_out = self.relu(decoder_out)
+
         # [sequence_len, batch_size, dimensions]
         params = self.dec2features(decoder_out)
 
@@ -105,31 +115,37 @@ def loss_normal2d(model_output, device, beta):
     # iterate over each time step in the sequence to compute NLL and KL terms
 
     t = 0
-    cov_matrix = torch.diag_embed(sigma[t, :, :])
     # define the distribution
-    p = distributions.MultivariateNormal(mu[t, :, :], cov_matrix)
-    log_prob = torch.mean(p.log_prob(x_true[t + 1, :, :]), dim=-1)
+    p = distributions.Normal(mu[t, :, :], sigma[t, :, :])
+    log_prob = torch.sum(p.log_prob(x_true[t + 1, :, :]), dim=-1)
     # dimensions [batch_size, dimension]
     ones_vector = torch.ones((z_mu.shape[1], z_mu.shape[2])).to(device)
     # KL-divergence
-    kl = 0.5 * torch.sum(ones_vector + z_log_var[t, :, :] - z_mu[t, :, :] ** 2 - torch.exp(z_log_var[t, :, :]), dim=-1)
+    negative_kl = 0.5 * torch.sum(ones_vector + z_log_var[t, :, :] - z_mu[t, :, :] ** 2 - torch.exp(z_log_var[t, :, :]),
+                                  dim=-1)
 
+    kl_tt = -1 * negative_kl
     for t in range(1, seq_length - 1):
         # print(t)
         # construct (diagonal) covariance matrix for each time step based on
         # the estimated var from the model
-        cov_matrix = torch.diag_embed(sigma[t, :, :])
+        #        cov_matrix = torch.diag_embed(sigma[t, :, :])
 
         # define the distribution
         #        p = distributions.Normal(mu[:, t, :], sigma[:, t, :])
-        p = distributions.MultivariateNormal(mu[t, :, :], cov_matrix)
+        #        p = distributions.MultivariateNormal(mu[t, :, :], cov_matrix)
 
-        log_prob += torch.mean(p.log_prob(x_true[t + 1, :, :]), dim=-1)
+        #        log_prob += torch.mean(p.log_prob(x_true[t + 1, :, :]), dim=-1)
+        p = distributions.Normal(mu[t, :, :], sigma[t, :, :])
+
+        log_prob += torch.sum(p.log_prob(x_true[t + 1, :, :]), dim=-1)
         # KL-divergence
-        kl += 0.5 * torch.sum(ones_vector + z_log_var[t, :, :] - z_mu[t, :, :] ** 2 -
-                              torch.exp(z_log_var[t, :, :]))
+        negative_kl += 0.5 * torch.sum(ones_vector + z_log_var[t, :, :] - z_mu[t, :, :] ** 2 -
+                                       torch.exp(z_log_var[t, :, :]), dim=-1)
 
-    NLL, KL = -torch.mean(log_prob, dim=0) / (seq_length - 1), -torch.mean(kl, dim=0) / (seq_length - 1)
+        kl_tt += -1* negative_kl
+    # average over the batches and divide by the sequence len - 1 to get the average over the sequence
+    NLL, KL = -torch.mean(log_prob, dim=0) / (seq_length - 1), -torch.mean(kl_tt, dim=0)/ (seq_length - 1)
 
     ELBO = NLL + beta * KL
 
@@ -174,7 +190,7 @@ def loss_normal2d_lognormal(model_output, device, beta):
                               torch.exp(z_log_var[t, :, :]))
 
     NLL, KL = -torch.mean(torch.mean(log_prob, dim=-1), dim=0) / (seq_length - 1), -torch.mean(kl, dim=0) / (
-                seq_length - 1)
+            seq_length - 1)
 
     ELBO = NLL + beta * KL
 

ts_anomaly_function.py
@@ -3,6 +3,8 @@
 Created on Fri Nov 29 15:22:42 2019
 
 """
+import math
+import numpy as np
 import torch
 from torch import distributions, Tensor
 
@@ -16,7 +18,7 @@ from torch import distributions, Tensor
 #      that is, a tensor of dimension T x 2*D
 # - prob_threshold: a number between 0 and 1. An observation with 
 #      probability < prob_threshold is labeled as an outlier
-def detect_anomalies(sequence, net, device, prob_threshold):
+def detect_anomalies(sequence, net, device, prob_threshold, std=False, k=0):
     with torch.no_grad():
         net.eval()
         # get it to the device and put the batch dimension
@@ -37,20 +39,32 @@ def detect_anomalies(sequence, net, device, prob_threshold):
         probs = []
         labels = [False] * mu.shape[0]
         for t in range(0, mu.shape[0] - 1):
-            cov_matrix = torch.diag(std_dev[t, :])
-            # define distribution with params estimated for time t+1
-            # in the original sequence (that's simply t in the params
-            # outputted by the model)
-            p = distributions.MultivariateNormal(mu[t, :], cov_matrix)
+            if std:
+                # use the standard deviation to find if all the points are outside k times the std
+                anomaly = False
+                for feature in range(mu.shape[1]):
+                    # if the signal is inside in just one feature it's not an anomaly
+                    if (prepared_sequence[0, t + 1, feature] > mu[t, feature] + k * std_dev[t, feature]) or \
+                            (prepared_sequence[0, t + 1, feature] < mu[t, feature] - k * std_dev[t, feature]):
+                        anomaly = True
+                        break
+                if anomaly:
+                    labels[t + 1] = True
+            else:
+                cov_matrix = torch.diag(std_dev[t, :])
+                # define distribution with params estimated for time t+1
+                # in the original sequence (that's simply t in the params
+                # outputted by the model)
+                p = distributions.MultivariateNormal(mu[t, :], cov_matrix)
 
-            # measure the probability of the observation at time t+1
-            # under the model and store the probability
-            probability = torch.exp(p.log_prob(prepared_sequence[0, t + 1, :])).cpu().detach().numpy()
-            probs.append(probability)
+                # measure the probability of the observation at time t+1
+                # under the model and store the probability
+                probability = torch.exp(p.log_prob(prepared_sequence[0, t + 1, :])).cpu().detach().numpy()
+                probs.append(probability)
 
-            # store outlier label
-            if probability < prob_threshold:
-                labels[t + 1] = True
+                # store outlier label
+                if probability < prob_threshold:
+                    labels[t + 1] = True
 
         # collect results in a dictionary
         outliers = {
@@ -60,8 +74,14 @@ def detect_anomalies(sequence, net, device, prob_threshold):
     return outliers
 
 
-def detect_anomalies_VAE(model_output, device, prob_threshold):
+def detect_anomalies_VAE(sequence, net, device, prob_threshold):
     with torch.no_grad():
+        net.eval()
+        # get it to the device and put the batch dimension
+        prepared_sequence = (sequence).to(device).unsqueeze(0)
+
+        # run the model
+        model_output = net(prepared_sequence, device)
         x_true = model_output["x_input"].permute(1, 0, 2)
         params = model_output["params"]
         mu, logvar = torch.chunk(params, 2, dim=-1)
@@ -85,9 +105,7 @@ def detect_anomalies_VAE(model_output, device, prob_threshold):
             cov_matrix = torch.diag_embed(sigma[t, :, :])
 
             # define the distribution
-            #        p = distributions.Normal(mu[:, t, :], sigma[:, t, :])
-            p = distributions.MultivariateNormal(mu[t, :, :], cov_matrix)
-
+            p = distributions.Normal(mu[t, :, :], sigma[t, :, :])
             log_prob = torch.mean(p.log_prob(x_true[t + 1, :, :]), dim=-1)
             # KL-divergence
             kl = - 0.5 * torch.sum(ones_vector + z_log_var[t, :, :] - z_mu[t, :, :] ** 2 -

ts_dataloader.py
@@ -39,92 +39,71 @@ class RealisticDataset(Dataset):
         return False
 
 
-def get_train_valid_test_signals(T, W, dataset_id, t_v_t_split, device, p=0.001):
-    ADDANOMALIES = False
-    train_valid_time = math.floor(t_v_t_split[0] * T)
-    valid_test_time = math.floor((t_v_t_split[0] + t_v_t_split[1]) * T)
-    if dataset_id == 0:
+def load_data(scenario, T):
+    if scenario == 0:
         signals = [
-            ("sinusoid", {"frequency": 0.0019}),
-            ("sinusoid", {"frequency": 0.005}),
-            ("sinusoid", {"frequency": 0.003}),
-            ("sinusoid", {"frequency": 0.0023}),
-            ("sinusoid", {"frequency": 0.01}),
-            ("sinusoid", {"frequency": 0.013}),
-            ("sinusoid", {"frequency": 0.087}),
-            ("sinusoid", {"frequency": 0.0007}),
+            ("sinusoid", {"frequency": 0.19}),
+            ("ar", {"ar_param": [0.8, 0.15], "sigma": 1}),
+            ("sinusoid", {"frequency": 0.5}),
+            ("sinusoid", {"frequency": 0.3}),
+            ("sinusoid", {"frequency": 0.23}),
+            ("sinusoid", {"frequency": 0.1}),
+            ("sinusoid", {"frequency": 0.3}),
+            ("sinusoid", {"frequency": 0.87}),
+            ("sinusoid", {"frequency": 0.07}),
             ("sinusoid", {"frequency": 0.03})
         ]
-        features = len(signals)
-
         # generate the time series using signals
-        timeseries_signals = generate_timeseries(signals, T=T)
-        timeseries_signals, timeseries_labels = insert_anomalies(timeseries_signals, magnitude=0.1)
+        signals = generate_timeseries(signals, T=T, noise_std=0.01)
 
+    elif scenario == 1:
+        # sine sequences
+        signals = [
+            ("sinusoid", {"frequency": 0.023, "amplitude": 2}),
+            #("sinusoid", {"frequency": 0.05, "amplitude": 2}),
+            #            ("gp", {"kernel": 'Periodic', "p": 50}),
+            ("ar", {"ar_param": [0.8, 0.15], "sigma": 1})
+        ]
+
+        # create the timeseries
+        signals = generate_timeseries(signals, T=T, noise_std=0.01,
+                                                 transforms=[#lambda x: 5 * np.sin(x),
+                                                             lambda x: x ** 3],
+                                                 #            #lambda x: 5 * np.sign(x)],
+                                                 transforms_std=[0.07, 0.03, 0.04])
+        #        timeseries_signals = generate_timeseries(signals, T=T, noise_std=0.001)
+    elif scenario == 2:
+        df_LA = pd.read_csv("LA.csv")
+        signals = df_LA.to_numpy()[:T, :]
+
+    else:
+        raise ValueError("Scenario not recognized, it should be 0, 1 or 2.")
+    return signals
+
+
+def get_datasets(scenario, t_v_t_split, W, device, signals, labels=None):
+    T = signals.shape[0]
+    features = signals.shape[1]
+    train_valid_time = math.floor(t_v_t_split[0] * T)
+    valid_test_time = math.floor((t_v_t_split[0] + t_v_t_split[1]) * T)
+    if (scenario == 0) or (scenario == 1):
         # normalize the signal
-        normalized_signals = (timeseries_signals - np.mean(timeseries_signals, axis=0)) / np.std(timeseries_signals,
-                                                                                                 axis=0)
+        normalized_signals = (signals - np.mean(signals, axis=0)) / np.std(signals, axis=0)
         # create train/valid/test split
         train_timeseries_signals = normalized_signals[0:train_valid_time]
         valid_timeseries_signals = normalized_signals[train_valid_time:valid_test_time]
         test_timeseries_signals = normalized_signals[valid_test_time:]
 
-        train_timeseries_labels = timeseries_labels[0:train_valid_time]
-        valid_timeseries_labels = timeseries_labels[train_valid_time:valid_test_time]
-        test_timeseries_labels = timeseries_labels[valid_test_time:]
+        train_timeseries_labels = labels[0:train_valid_time]
+        valid_timeseries_labels = labels[train_valid_time:valid_test_time]
+        test_timeseries_labels = labels[valid_test_time:]
 
         train_dataset = SyntheticDataset([train_timeseries_signals, train_timeseries_labels], features, window_size=W,
                                          device=device)
         valid_dataset = SyntheticDataset([valid_timeseries_signals, valid_timeseries_labels], features, device=device)
         test_dataset = SyntheticDataset([test_timeseries_signals, test_timeseries_labels], features, device=device)
-
-        print("Dataset created.")
-    elif dataset_id == 1:
-        # sine sequences
-        signals = [
-            #("sinusoid", {"frequency": 0.025}),
-#            ("gp", {"kernel": 'Periodic', "p": 50}),
-            ("ar", {"ar_param": [0.8, 0.15], "sigma": 1})
-        ]
-
-        # create the timeseries
-        timeseries_signals = generate_timeseries(signals, T=T, noise_std=0.01,
-                                                 transforms=[lambda x: x ** 2,
-                                                             lambda x: x ** 2,
-                                                             lambda x: 10*np.sign(x)],
-                                                 transforms_std=[0.007, 0.003, 0.004])
-#        timeseries_signals = generate_timeseries(signals, T=T, noise_std=0.001)
-        features = timeseries_signals.shape[1]
-
-        import matplotlib.pyplot as plt
-        plt.plot(range(len(timeseries_signals)), timeseries_signals)
-        timeseries_signals = (timeseries_signals - np.mean(timeseries_signals, axis=0)) / np.std(timeseries_signals,
-                                                                                                 axis=0)
-
-        timeseries_signals, timeseries_labels = insert_anomalies(timeseries_signals, magnitude=5, p=p)
-
-        normalized_signals = (timeseries_signals - np.mean(timeseries_signals, axis=0)) / np.std(timeseries_signals,
-                                                                                                 axis=0)
-
-        train_timeseries_signals = normalized_signals[0:train_valid_time]
-        valid_timeseries_signals = normalized_signals[train_valid_time:valid_test_time]
-        test_timeseries_signals = normalized_signals[valid_test_time:]
-
-        train_timeseries_labels = timeseries_labels[0:train_valid_time]
-        valid_timeseries_labels = timeseries_labels[train_valid_time:valid_test_time]
-        test_timeseries_labels = timeseries_labels[valid_test_time:]
-
-        train_dataset = SyntheticDataset([train_timeseries_signals, train_timeseries_labels], features, window_size=W,
-                                         device=device)
-        valid_dataset = SyntheticDataset([valid_timeseries_signals, valid_timeseries_labels], features, device=device)
-        test_dataset = SyntheticDataset([test_timeseries_signals, test_timeseries_labels], features, device=device)
-
-        print("Dataset created.")
-    elif dataset_id == 2:
-
-        df_LA = pd.read_csv("LA.csv")
+    elif scenario == 2:
         features = 3
-        signals = df_LA.to_numpy()
         normalized_signals = (signals - np.mean(signals, axis=0)) / np.std(signals, axis=0)
         train_dataset = RealisticDataset(normalized_signals[0:train_valid_time, :3], features, window_size=W,
                                          device=device)
@@ -133,9 +112,12 @@ def get_train_valid_test_signals(T, W, dataset_id, t_v_t_split, device, p=0.001)
                                          device=device)
         test_dataset = RealisticDataset(normalized_signals[valid_test_time:, :3], features, window_size=W,
                                         device=device)
+    else:
+        train_dataset = None
+        valid_dataset = None
+        test_dataset = None
 
-    # set up the DataLoader
-    # This dataloader will load data with shape [batch_size, time_length, features]
+
     return features, \
            train_dataset, \
            valid_dataset, \

ts_simple_LSTM_net.py
@@ -17,6 +17,7 @@ class Standard_LSTM(nn.Module):
         self.lstm = nn.LSTM(input_dimension, hidden_dim, num_layers=2)
         self.hidden2hidden = nn.Linear(hidden_dim, hidden_dim)
         self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(0.75)
         self.hidden2params = nn.Linear(hidden_dim, param_size * input_dimension)
 
     def forward(self, x, device):
@@ -29,7 +30,7 @@ class Standard_LSTM(nn.Module):
         lstm_out, (h, c) = self.lstm(x)
         # linear wants [batch, seq_len, hidden_size]
         # lstm_out = lstm_out.permute(1, 0, 2)
-        linear_in = self.relu(self.hidden2hidden(lstm_out))
+        linear_in = self.dropout(self.relu(self.hidden2hidden(lstm_out)))
 
         # take output of hidden layers at each time step h_t and run it through a fully connected layer
         params = self.hidden2params(linear_in)

ts_training.py
@@ -25,9 +25,88 @@ def anomaly_detection_accuracy(ground_truth, predictions):
         "total": total  # total number of datapoints in the sequence
     }
 
+def plot_LSTM(valid_dataset, train_loss, valid_loss, step_valid_loss, valid_accuracy):
+    is_labelled = valid_dataset.has_labels()
+    fig = plt.figure(figsize=(20, 7))
+    num_plots = 2 if is_labelled else 1
+    ax1 = plt.subplot(1, num_plots, 1)
+    ax1.plot(range(len(train_loss)), train_loss, label="Training loss")
+    ax1.plot(np.concatenate((np.array([0]), (np.array(range(1,len(valid_loss))) * step_valid_loss)-1)), valid_loss, label="Validation loss")
+    ax1.set_xlabel("Epoch")
+    ax1.set_ylabel("Loss")
+    ax1.legend()
+
+    if is_labelled:
+        ax2 = plt.subplot(1, 3, 3)
+        ax2.plot(np.array(range(len(valid_accuracy["total"]))) * 10,
+                 valid_accuracy["correct"] / valid_accuracy["total"],
+                 label="Correctly labelled signals")
+        ax2.set_xlabel("Epoch")
+        ax2.set_ylabel("Percentage")
+        ax2.legend(loc="upper left")
+    return fig
+
+def plot_VAE(valid_dataset, train_KL, train_loss, valid_loss, step_valid_loss, valid_accuracy, z):
+    is_labelled = valid_dataset.has_labels()
+    #has_2_latent = True if z.shape[2] == 2 else False
+    latent_features = z.shape[2]
+    fig = plt.figure(figsize=(20, (latent_features+1)*8), constrained_layout=True)
+
+    if is_labelled:
+        num_plots = (latent_features+1, 3)
+    else:
+        num_plots = (latent_features+1, 2)
+
+
+    gs = fig.add_gridspec(*num_plots)
+
+    cur_plot = 0
+    ax1 = fig.add_subplot(gs[0,cur_plot])
+    ax1.set_title("ELBO")
+    ax1.plot(range(len(train_loss)), train_loss, label="Training ELBO")
+    ax1.plot(np.concatenate((np.array([0]), (np.array(range(1,len(valid_loss))) * step_valid_loss)-1)), valid_loss, label="Validation ELBO")
+    ax1.set_xlabel("Epoch")
+    ax1.set_ylabel("ELBO")
+    ax1.legend()
+
+    cur_plot += 1
+    ax2 = fig.add_subplot(gs[0,cur_plot])
+    ax2.set_title("KL divergence")
+    kl, = ax2.plot(range(len(train_KL)), train_KL, label="Training KL")
+    ax2.set_xlabel("Epoch")
+    ax2.set_ylabel("KL")
+    ax2.legend()
+
+    cur_plot += 1
+    if is_labelled:
+        ax3 = fig.add_subplot(gs[0,cur_plot])
+        ax3.set_title("Correctly labelled signals")
+        ax3.plot(np.array(range(len(valid_accuracy["total"]))) * 10,
+                 valid_accuracy["correct"] / valid_accuracy["total"],
+                 label="Correctly labelled signals")
+        ax3.set_xlabel("Epoch")
+        ax3.set_ylabel("Percentage")
+        ax3.legend(loc="upper left")
+
+    cur_row = 1
+    for latent_feature in range(latent_features):
+        ax4 = fig.add_subplot(gs[cur_row,:])
+        ax4.set_title("Latent feature {}".format(latent_feature))
+        #color = np.linspace(0,1,z.shape[0])
+        #ax4.scatter(*z[:,0,:].reshape(-1,2).T, c = color, label = "Darker color, bigger time")
+        ax4.plot(list(range(z.shape[0])), z[:, 0, latent_feature])
+        ax4.set_xlabel("Time")
+        ax4.set_ylabel("Amplitude")
+        cur_row += 1
+
+    return fig
 
 def train_network(device, train_loader, valid_dataset, epochs, net, loss_function, optimizer, beta_annealing=None,
                   scheduler=None, plotting=True, p_anomaly=0.01):
+    torch.manual_seed(42)
+    np.random.seed(42)
+
+    PLOT_STEP = 10
     train_loss = []
     train_KL = []
     print("Training has started.")
@@ -55,10 +134,11 @@ def train_network(device, train_loader, valid_dataset, epochs, net, loss_functio
     train_accuracy = {
 
     }
+    z = None
     # cycle through the epochs
     for epoch in range(epochs):
         if plotting:
-            if epoch % 10 == 0:
+            if ((epoch+1) % PLOT_STEP == 0) or (epoch == 0):
                 print("Epoch: {}".format(epoch))
 
         batch_loss = []
@@ -69,7 +149,7 @@ def train_network(device, train_loader, valid_dataset, epochs, net, loss_functio
 
         for batch_idx, x in enumerate(train_loader):
             net.zero_grad()
-
+            #optimizer.zero_grad()
             # shape: [batch_size, seq_len, features]
             input = x[0]  # x[0] is the data x[1] is the labels
 
@@ -86,6 +166,7 @@ def train_network(device, train_loader, valid_dataset, epochs, net, loss_functio
             batch_loss.append(loss_params["loss"].item())
             # if there is a KL divergence, add it
             if "KL" in loss_params.keys():
+                z = output_model["z"].cpu().detach()
                 batch_KL.append(loss_params["KL"].item())
 
             # BACKPROPAGATION
@@ -98,7 +179,7 @@ def train_network(device, train_loader, valid_dataset, epochs, net, loss_functio
         if len(batch_KL) > 0:
             train_KL.append(np.mean(batch_KL))
         # every 10 epochs
-        if epoch % 10 == 0:
+        if ((epoch+1) % PLOT_STEP == 0) or epoch == 0:
             net.eval()
             with torch.no_grad():
                 # forward pass using the validation sequence
@@ -115,9 +196,9 @@ def train_network(device, train_loader, valid_dataset, epochs, net, loss_functio
                 if valid_dataset.has_labels():
                     # detect the anomalies
                     if beta_annealing is None:
-                        anomaly_data = detect_anomalies(valid_sequence, net, device, p_anomaly)
+                        anomaly_data = detect_anomalies(valid_sequence, net, device, p_anomaly, False)
                     else:
-                        anomaly_data = detect_anomalies_VAE(output_model, device, p_anomaly)
+                        anomaly_data = detect_anomalies_VAE(valid_sequence, net, device, p_anomaly)
                     # what we predict is an anomaly
                     predictions = anomaly_data["outlier_label"]
                     # find the accuracy of the prediction
@@ -132,7 +213,9 @@ def train_network(device, train_loader, valid_dataset, epochs, net, loss_functio
             if plotting:
 
                 if len(train_KL) == 0:
-                    plt.figure(figsize=(20, 7))
+                    plot_LSTM(valid_dataset, train_loss, valid_loss, PLOT_STEP, valid_accuracy)
+                    plt.show()
+                    """plt.figure(figsize=(20, 7))
                     num_plots = 2 if valid_dataset.has_labels() else 1
                     ax1 = plt.subplot(1, num_plots, 1)
                     ax1.plot(range(len(train_loss)), train_loss, label="Training loss")
@@ -149,9 +232,11 @@ def train_network(device, train_loader, valid_dataset, epochs, net, loss_functio
                         ax2.set_xlabel("Epoch")
                         ax2.set_ylabel("Percentage")
                         ax2.legend(loc = "upper left")
-                    plt.show()
+                    plt.show()"""
                 else:
-                    plt.figure(figsize=(20, 10))
+                    plot_VAE(valid_dataset, train_KL, train_loss, valid_loss, PLOT_STEP, valid_accuracy,z)
+                    plt.show()
+                    """plt.figure(figsize=(20, 10))
                     num_plots = 3 if valid_dataset.has_labels() else 2
                     ax1 = plt.subplot(1, num_plots, 1)
                     ax1.plot(range(len(train_loss)), train_loss, label="Training loss")
@@ -172,14 +257,17 @@ def train_network(device, train_loader, valid_dataset, epochs, net, loss_functio
                         ax3.set_xlabel("Epoch")
                         ax3.set_ylabel("Percentage")
                         ax3.legend(loc="upper left")
-                    plt.show()
+                    plt.show()"""
 
         # step in the scheduler and in the annealing of beta
         if scheduler != None:
             scheduler.step()
         if beta_annealing != None:
             beta = beta_annealing(beta, epoch)
-    if (plotting):
-        plt.plot(range(len(train_loss)), train_loss)
-        plt.show()
-    return net.state_dict()
+
+    if len(train_KL) == 0:
+        fig = plot_LSTM(valid_dataset, train_loss, valid_loss, PLOT_STEP, valid_accuracy)
+    else:
+        fig = plot_VAE(valid_dataset, train_KL, train_loss, valid_loss, PLOT_STEP, valid_accuracy, z)
+
+    return net.state_dict(), fig